eb219ce8f61953eb46e45ef32bc4cb776d1d3a5c
nobody <seekrr@qq.com>
2018-08-30 00:04:15 +0800

    定制default模板

--- templates/js-template-default/frameworks/runtime-src/proj.android/jni/Android.mk
+++ templates/js-template-default/frameworks/runtime-src/proj.android/jni/Android.mk
@@ -1,6 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
+# --- bugly: 引用 libBugly.so ---
 include $(CLEAR_VARS)
+LOCAL_MODULE := bugly_native_prebuilt
+LOCAL_SRC_FILES := prebuilt/$(TARGET_ARCH_ABI)/libBugly.so
+include $(PREBUILT_SHARED_LIBRARY)
+# --- bugly: end ---
 
 LOCAL_MODULE := cocos2djs_shared
 
@@ -10,17 +15,23 @@ ifeq ($(USE_ARM_MODE),1)
 LOCAL_ARM_MODE := arm
 endif
 
-LOCAL_SRC_FILES := hellojavascript/main.cpp \
-				   ../../Classes/AppDelegate.cpp \
-				   ../../Classes/jsb_module_register.cpp \
+# --- bugly: 增加cpp扩展名mm
+LOCAL_CPP_EXTENSION := .mm .cpp .cc
+LOCAL_CFLAGS += -x c++
+
+LOCAL_SRC_FILES := \
+../../Classes/AppDelegate.cpp \
+../../Classes/jsb_module_register.cpp \
+hellojavascript/main.cpp \
+../../Classes/bugly/CrashReport.mm \
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
 ifeq ($(USE_ANY_SDK),1)
 LOCAL_SRC_FILES += ../../Classes/anysdk/SDKManager.cpp \
-				   ../../Classes/anysdk/jsb_anysdk_basic_conversions.cpp \
-				   ../../Classes/anysdk/manualanysdkbindings.cpp \
-				   ../../Classes/anysdk/jsb_anysdk_protocols_auto.cpp
+	../../Classes/anysdk/jsb_anysdk_basic_conversions.cpp \
+	../../Classes/anysdk/manualanysdkbindings.cpp \
+	../../Classes/anysdk/jsb_anysdk_protocols_auto.cpp
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Classes/anysdk
 
